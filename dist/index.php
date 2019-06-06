1f527a8678ee38f2e9124fd573019316ea7e17bc
Steven Morris <steven.morris@reckless.agency>
2019-06-06 14:30:48 +0100

    More Components work

--- dist/index.php
+++ dist/index.php
@@ -2,13 +2,26 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Title</title>
     <link rel="stylesheet" href="css/app.css">
     <script src="js/app.js"></script>
+
 </head>
 <body>
 <?php include '../frontend-snippets/components/hero-full-screen/hero-full-screen.html'; ?>
 <?php include '../frontend-snippets/components/hero-internal/hero-internal.html'; ?>
+<?php include '../frontend-snippets/components/page-title/page-title.html'; ?>
+<div class="container">
+    <div class="row">
+        <div class="col-12 col-lg-4">
+            <?php include '../frontend-snippets/components/card/article/card-article.html'; ?>
+        </div>
+    </div>
+</div>
+
 
 </body>
 </html>
