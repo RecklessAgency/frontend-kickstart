19d4d3a80aebf353a8cbaa7f23d43fdceacde447
Daniel Dewhurst <daniel.dewhurst@reckless.agency>
2019-06-19 10:59:21 +0100

    General clean up

--- src/js/app.js
+++ src/js/app.js
@@ -28,9 +28,3 @@ require('./bootstrap');
 // const app = new Vue({
 //   el: '#app'
 // });
-
-// Components
-require('slick-carousel');
-require('../../frontend-snippets/components/hero-full-screen/hero-full-screen');
-require('../../frontend-snippets/components/hero-internal/hero-internal');
-require('../../frontend-snippets/components/card/block/card-block');
